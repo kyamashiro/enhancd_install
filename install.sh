d932ef26015eea79746f98253f67fd90ac1c7e20
kyamashiro <36433535+kyamashiro@users.noreply.github.com>
2020-02-05 10:17:19 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+set -e
+
+if [ "`whoami`" == "root" ]; then
+  echo "Do not run with root privileges"
+  exit 1
+fi
+
+sudo yum install wget -y
+cd ~
+wget "https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_386.tar.gz"
+tar xzvf peco_linux_386.tar.gz
+chmod +x peco_linux_386/peco
+sudo cp peco_linux_386/peco /usr/local/bin
+git clone https://github.com/b4b4r07/enhancd
+echo source ~/enhancd/init.sh >> .bashrc
+rm peco_linux_386.tar.gz
+rm -rf peco_linux_386
+exec $SHELL -l
