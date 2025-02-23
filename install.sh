4eeface87393e1084d761c8c488e11945e517c76
kyamashiro <36433535+kyamashiro@users.noreply.github.com>
2025-02-23 09:22:35 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ fi
 
 sudo apt install wget -y
 cd ~
-wget "https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_386.tar.gz"
-tar xzvf peco_linux_386.tar.gz
-chmod +x peco_linux_386/peco
-sudo cp peco_linux_386/peco /usr/local/bin
+wget "https://github.com/peco/peco/releases/download/v0.5.11/peco_linux_amd64.tar.gz"
+tar xzvf peco_linux_amd64.tar.gz
+chmod +x peco_linux_amd64/peco
+sudo cp peco_linux_amd64/peco /usr/local/bin
 git clone https://github.com/b4b4r07/enhancd
 echo source ~/enhancd/init.sh >> .zshrc
-rm peco_linux_386.tar.gz
-rm -rf peco_linux_386
+rm peco_linux_amd64.tar.gz
+rm -rf peco_linux_amd64
 exec $SHELL -l
